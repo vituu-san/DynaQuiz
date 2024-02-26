16258f3158a88a8c76fd797d90f20c4029396fcd
Vitor Costa <developer.deivide.costa@gmail.com>
2024-02-26 16:29:16 -0300

    add project

---
+++ DynaQuiz/DynaQuiz/DynaQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynaQuizApp.swift
+//  DynaQuiz
+//
+//  Created by Vitor Costa on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DynaQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
